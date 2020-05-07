a32a9f8482bd1f2cb1bd109deb87af909838d0d7
misslavada <56700239+misslavada@users.noreply.github.com>
2020-05-06 17:40:13 -0700

    Add files via upload

---
+++ part3/JSON.js
@@ -0,0 +1,27 @@
+{"animals"} [	
+    {		
+    "name":"Endo",		
+    "title":"Doggo",		
+    "bio":"He's my dog and best pal"	
+    },
+    {		
+    "name":"Lemon",		
+    "title":"An absolute unit of a cat",		
+    "bio":"Fat ass cat"	
+    },
+    {		
+    "name":"Harley",		
+    "title":"Chonky pupper",		
+    "bio":"Roomates dog"	
+    },
+    {		
+    "name":"Leia",		
+    "title":"Killer Kitty",		
+    "bio":"This cat cathes hummingbirds!"	
+    },
+    {		
+    "name":"Rhaegar",		
+    "title":"My first kitty",		
+    "bio":"Deceased but never forgotten"	
+    },	
+]
